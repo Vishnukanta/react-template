bf38b7a34b575f1c4aabc0cf340cfb707c0f4dfe
MacBook Air <nazariipirih@MacBook-Air.local>
2023-06-11 12:17:45 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return React.createElement('h1', {}, 'Hello Vishnukanta Das')
+  return e('div', { className: 'container' }, [
+    e('h1', { className: 'text-red-600', key: 1 }, 'Test Vishnukanta'),
+    e('button', { className: 'py-2 px-4 border', key: 2 }, 'Click me'),
+  ])
 }
 
-export default App;
+export default App
